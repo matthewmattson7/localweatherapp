7f1d6697acd3f2c0bff1b5ec94a3a768565eb931
matthewmattson7 <matthewmattson7@gmail.com>
2017-10-03 23:57:19 -0400

    Added files from local machine
    ^^^^

---
+++ app.js
@@ -0,0 +1,34 @@
+
+weather();
+
+function weather() {
+
+    var location = document.getElementById("location");
+    var apiKey = 'd9de84297f1f36980dfc9dda0bf950cf';
+    var url = 'https://api.forecast.io/forecast/';
+
+    navigator.geolocation.getCurrentPosition(success, error);
+
+    function success(position) {
+      latitude = position.coords.latitude;
+      longitude = position.coords.longitude;
+
+      location.innerHTML = 'Latitude is ' + latitude + '° Longitude is ' + longitude + '°';
+
+       $.getJSON(url + apiKey + "/" + latitude + "," + longitude + "?callback=?", function(data) {
+        $('#temp').html(data.currently.temperature + '° F');
+        $('#minutely').html(data.minutely.summary);
+      });
+    }
+
+    function error() {
+  location.innerHTML = "Unable to retrieve your location";
+}
+
+location.innerHTML = "Locating...";
+
+
+
+}
+
+weather();
